1c0df6d0ca0884e21a90cd64f97ede07eba87662
Adrian Katulski <katulskiadrian@gmail.com>
2020-05-04 19:29:58 +0200

    dodanie klasy do przetwornicy step down

---
+++ converterDC/converter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace converterDC
+{
+    int frequency;
+    double maxInputVoltage, minInputVoltage, inputVoltage;
+    double maxOutputVoltage, minOutputVoltage, outputVoltage;
+    double maxOutputCurrent, outputCurrent;
+    class converter
+    {
+    }
+}
